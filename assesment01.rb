68123747d76ffd53b80181a83c9b4d035d23d5ea
poorvashelke <32210401+poorvashelke@users.noreply.github.com>
2019-06-04 22:30:24 -0700

    Update assesment01.rb

--- assesment01.rb
+++ assesment01.rb
@@ -67,6 +67,10 @@
     end
   end
   #_____________________________________________________________
+  # Write a String#symmetric_substrings method that returns an array of substrings
+  # that are palindromes, e.g. "cool".symmetric_substrings => ["oo"]
+  # Only include substrings of length > 1.
+
   def symmetric_substrings
       symm_subs = []
 
